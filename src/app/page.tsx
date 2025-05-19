90896183c5fc891ce8dafe023ba1ce18a6cf159f
Alex Cantón <alexcantongarcia97@gmail.com>
2025-05-19 22:11:27 +0200

    Add About me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import AboutSection from "@/components/sections/AboutSection";
 import HeroSection from "@/components/sections/HeroSection";
+import TestSection from "@/components/sections/TestSection";
 
 const Home = () => (
 	<>
 		<HeroSection />
 		<AboutSection />
+		<TestSection />
 	</>
 );
 
--- src/components/shared/NavbarDesktop.tsx
+++ src/components/shared/NavbarDesktop.tsx
@@ -14,7 +14,7 @@ const NavbarDesktop = () => {
 
 	return (
 		<div className="w-full flex flex-row justify-center items-center pt-6 fixed top-0 left-0 right-0 z-50">
-			<div className="bg-[#011627]/40 text-[#00c896] w-3/4 xl:w-1/2 h-16 flex flex-row items-center justify-between rounded-xl backdrop-blur-md border border-transparent animated-border pulse-shadow px-4">
+			<div className="bg-[#011627]/70 text-[#00c896] w-3/4 xl:w-1/2 h-16 flex flex-row items-center justify-between rounded-xl backdrop-blur-md border border-transparent animated-border pulse-shadow px-4">
 				<Link href="/" className="flex items-center gap-2">
 					<Image
 						src={Logo}
