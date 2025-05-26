398ef55d36d5462701c4be7650245242f1d1d2a6
Alex Cantón <alexcantongarcia97@gmail.com>
2025-05-26 22:12:56 +0200

    Finish blog page and post page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,12 +8,12 @@ const BlogPage = async () => {
 	const files = await getAllFileSortedByDate();
 
 	return (
-		<div className="w-full flex flex-col justify-center items-center gap-10">
-			<div className="w-3/4 flex flex-col gap-2">
+		<div className="w-full flex flex-col justify-center items-center gap-10 px-4 lg:px-0">
+			<div className="w-full lg:w-3/4 flex flex-col gap-2">
 				<Heading level={1} className="text-6xl uppercase">{t("title")}</Heading>
 				<p className="text-lg">{t("text")}</p>
 			</div>
-			<div className="w-3/4 grid grid-cols-2 lg:grid-cols-3 gap-4">
+			<div className="w-full lg:w-3/4 grid grid-cols-1 lg:grid-cols-3 gap-4">
 				{files.map((file) => <PostCard key={file.slug} post={file} />)}
 			</div>
 		</div>
