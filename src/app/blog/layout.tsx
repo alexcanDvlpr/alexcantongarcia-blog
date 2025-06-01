8e302a309a43666b3ab5c6168fb8686c9ede9f16
Alex Cantón <alexcantongarcia97@gmail.com>
2025-06-01 15:21:56 +0200

    Add and finish footer

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,3 +1,4 @@
+import ContainerBackground from "@/components/shared/ContainerBackground";
 import { domain } from "@/shared";
 import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
@@ -38,12 +39,12 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default function BlogMdxLayout({ children }: { children: ReactNode }) {
 	return (
-		<div className="w-full min-h-screen py-24 lg:py-30 flex flex-col justify-start items-center bg-gray-900 text-white relative">
+		<ContainerBackground>
 			{/* Grid Background */}
 			<div className="absolute inset-0 opacity-65">
 				<div className="h-full w-full bg-grid-pattern bg-[length:40px_40px]"></div>
 			</div>
 			<div className="w-full z-20">{children}</div>
-		</div>
+		</ContainerBackground>
 	);
 }
