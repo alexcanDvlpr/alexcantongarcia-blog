6dac53da05ad71a2add251baa0ece12c3adc0255
Alex Cantón <alexcantongarcia97@gmail.com>
2025-06-15 16:10:56 +0200

    Restore middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,18 @@
+// middleware.ts
 import { NextRequest, NextResponse } from "next/server";
-import { locales } from "./i18n/request";
 
 export function middleware(request: NextRequest) {
 	const pathname = request.nextUrl.pathname;
 
 	if (pathname === "/quien-soy") {
-		const locale = request.cookies.get("NEXT_LOCALE")?.value || locales[1];
+		const locale = request.cookies.get("NEXT_ALEXCAN_LOCALE")?.value || "es";
 		if (locale === "en") {
 			return NextResponse.redirect(new URL("/about-me", request.url));
 		}
 	}
 
-    if (pathname === "/about-me") {
-		const locale = request.cookies.get("NEXT_LOCALE")?.value || locales[0];
+	if (pathname === "/about-me") {
+		const locale = request.cookies.get("NEXT_ALEXCAN_LOCALE")?.value || "en";
 		if (locale === "es") {
 			return NextResponse.redirect(new URL("/quien-soy", request.url));
 		}
